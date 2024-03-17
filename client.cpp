25047dee81cc6d5b4f32dc866cbc58b9e5c78170
Doguhan Akgunes <137198411+dovuhan@users.noreply.github.com>
2024-03-17 19:22:22 +0600

    Update client.cpp
    Changes Made:
    
    Error Handling:
    
    In the read_handler and connect_handler functions, std::cerr is used to print error messages on error conditions.
    In resolve_handler function, std::cerr is used to print error messages on error conditions.
    
    Code Layout:
    Indentation and spaces have been edited to make the code more readable.
    
    Note: This code connects to Google and retrieves the HTML content. You will need to modify the code for a different URL or a different HTTP request.

--- client.cpp
+++ client.cpp
@@ -14,36 +14,36 @@ tcp::resolver resolv{ioservice};
 tcp::socket tcp_socket{ioservice};
 std::array<char, 4096> bytes;
 
-void read_handler(const boost::system::error_code &ec, std::size_t bytes_transferred)
-{
-	if(!ec)
-	{
-		std::cout.write(bytes.data(), bytes_transferred);
-		tcp_socket.async_read_some(buffer(bytes), read_handler);
-	}
+void read_handler(const boost::system::error_code &ec, std::size_t bytes_transferred) {
+  if (!ec) {
+    std::cout.write(bytes.data(), bytes_transferred);
+    tcp_socket.async_read_some(buffer(bytes), read_handler);
+  } else {
+    std::cerr << "Read error: " << ec.message() << std::endl;
+  }
 }
 
-void conenct_handler(const boost::system::error_code &ec)
-{
-	if(!ec)
-	{
-		std::string r = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
-		write(tcp_socket, buffer(r));
-		tcp_socket.async_read_some(buffer(bytes), read_handler);
-	}
+void connect_handler(const boost::system::error_code &ec) {
+  if (!ec) {
+    std::string request = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
+    write(tcp_socket, buffer(request));
+    tcp_socket.async_read_some(buffer(bytes), read_handler);
+  } else {
+    std::cerr << "Connection error: " << ec.message() << std::endl;
+  }
 }
 
-void resolve_handler(const boost::system::error_code &ec, tcp::resolver::iterator it)
-{
-	if(!ec)
-	{
-		tcp_socket.async_connect(*it, conenct_handler);
-	}
+void resolve_handler(const boost::system::error_code &ec, tcp::resolver::iterator it) {
+  if (!ec) {
+    tcp_socket.async_connect(*it, connect_handler);
+  } else {
+    std::cerr << "Resolve error: " << ec.message() << std::endl;
+  }
 }
 
-int main()
-{
-	tcp::resolver::query q{"google.com", "80"};
-	resolv.async_resolve(q, resolve_handler);
-	ioservice.run();
+int main() {
+  tcp::resolver::query query{"google.com", "80"};
+  resolv.async_resolve(query, resolve_handler);
+  ioservice.run();
+  return 0;
 }
